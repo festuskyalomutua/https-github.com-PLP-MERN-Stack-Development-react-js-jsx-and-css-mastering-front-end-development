ae3c059a125a74e3b1df3e04ecfa977366ac7548
Festus Kyalo Mutua <festusk54@gmail.com>
2025-10-30 21:00:18 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // 👇 VERY IMPORTANT: this must match your GitHub repo name
-  base: '/react-js-jsx-and-css-mastering-front-end-development-festuskyalomutua/',
-})
